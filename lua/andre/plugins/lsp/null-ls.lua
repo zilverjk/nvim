b99621ffdb05deb46ca438d70b2b7b60b92e2059
André Cruz <acruztalavera@gmail.com>
2023-09-06 11:12:55 -0500

    Modificación de config null-ls para eslint-lsp

--- lua/andre/plugins/lsp/null-ls.lua
+++ lua/andre/plugins/lsp/null-ls.lua
@@ -26,7 +26,7 @@ return {
           extra_filetypes = { "svelte" },
         }), -- js/ts formatter
         formatting.stylua, -- lua formatter
-        diagnostics.eslint_d.with({ -- js/ts linter
+        diagnostics.eslint.with({ -- js/ts linter
           condition = function(utils)
             return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" }) -- only enable if root has .eslintrc.js or .eslintrc.cjs
           end,
