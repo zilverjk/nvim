f5252c018d3416a66393627935734745192afdf8
André Cruz <acruztalavera@gmail.com>
2024-02-29 12:35:23 -0500

    Se eliminó formatting y diagnostics nativo de null_ls por estar deprecado para usar none-ls-extras

--- lua/andre/plugins/lsp/none-ls.lua
+++ lua/andre/plugins/lsp/none-ls.lua
@@ -1,5 +1,6 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = { "nvimtools/none-ls-extras.nvim" },
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     local null_ls = require("null-ls")
@@ -8,13 +9,16 @@ return {
     local formatting = null_ls.builtins.formatting
     local diagnostics = null_ls.builtins.diagnostics
 
+    local eslintDiagnostics = require("none-ls.diagnostics.eslint")
+    local eslintFormatting = require("none-ls.formatting.eslint")
+
     local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
     null_ls.setup({
       root_dir = null_ls_utils.root_pattern(".null-ls-root", "Makefile", ".git", "package.json"),
       sources = {
-        diagnostics.eslint,
-        formatting.eslint.with({
+        eslintDiagnostics,
+        eslintFormatting.with({
           condition = function(utils)
             return utils.root_has_file(".eslintrc.js") and not utils.root_has_file(".prettierrc")
           end,
@@ -24,15 +28,15 @@ return {
             return utils.root_has_file(".prettierrc")
           end,
           extra_filetypes = { "svelte" },
-        }), -- js/ts formatter
-        formatting.stylua, -- lua formatter
+        }),
+        formatting.stylua,
         -- Python
         formatting.black,
         diagnostics.mypy,
         -- Golang
-        formatting.gofumpt, -- go install -v github.com/incu6us/goimports-reviser/v3@latest
+        formatting.gofumpt,           -- go install -v github.com/incu6us/goimports-reviser/v3@latest
         formatting.goimports_reviser, -- go install mvdan.cc/gofumpt@latest
-        formatting.golines, -- go install github.com/segmentio/golines@latest
+        formatting.golines,           -- go install github.com/segmentio/golines@latest
       },
       -- configure format on save
       on_attach = function(client, bufnr)
