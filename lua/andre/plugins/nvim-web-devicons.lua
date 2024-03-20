8f67cbd1c4ed490dfe3d1f9993d21b37f3fd0159
André Cruz <acruztalavera@gmail.com>
2024-03-20 18:22:10 -0500

    customizando iconos y sus colores

--- lua/andre/plugins/nvim-web-devicons.lua
+++ lua/andre/plugins/nvim-web-devicons.lua
@@ -2,11 +2,29 @@ return {
   "nvim-tree/nvim-web-devicons",
   config = function()
     require("nvim-web-devicons").set_icon({
+      ts = {
+        icon = "",
+        color = "#0288D1",
+        cterm_color = "67",
+        name = "TypeScript",
+      },
+      js = {
+        icon = "",
+        color = "#FFCA28",
+        cterm_color = "67",
+        name = "JavaScript",
+      },
       gql = {
-        icon = "",
+        icon = "󰡷",
         color = "#e535ab",
         cterm_color = "199",
         name = "GraphQL",
+      },
+      [".gitignore"] = {
+        icon = "",
+        color = "#E64A19",
+        cterm_color = "166",
+        name = "Git",
       },
     })
   end,
