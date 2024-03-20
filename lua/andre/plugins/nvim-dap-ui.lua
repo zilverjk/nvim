b3a15576e2fc45eceff2ce97acfa054bf1056d2b
André Cruz <acruztalavera@gmail.com>
2024-03-20 15:59:27 -0500

    nvim-dap necesita nvim-nio como dependencia

--- lua/andre/plugins/nvim-dap-ui.lua
+++ lua/andre/plugins/nvim-dap-ui.lua
@@ -1,7 +1,7 @@
 return {
   "rcarriga/nvim-dap-ui",
   event = "VeryLazy",
-  dependencies = "mfussenegger/nvim-dap",
+  dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
   config = function()
     vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "", linehl = "", numhl = "" })
     vim.fn.sign_define("DapStopped", { text = "➡️", texthl = "", linehl = "", numhl = "" })
