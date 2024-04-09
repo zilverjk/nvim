b75a5d79d551ec4fe85c9fd7c83c4f741b0494b9
André Cruz <acruztalavera@gmail.com>
2024-04-09 14:03:25 -0500

    Se restauró la config inicial custom que tenía de lualine

--- lua/andre/plugins/lualine.lua
+++ lua/andre/plugins/lualine.lua
@@ -1,142 +1,74 @@
 return {
   "nvim-lualine/lualine.nvim",
-  event = "VimEnter",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    local highlight = require("lualine.highlight")
-    local colors = require("andre.core.colors").colors
-    local fname = require("lualine.components.filename"):extend()
-    local modules = require("lualine_require").lazy_require({ utils = "lualine.utils.utils" })
-    local icons = require("nvim-web-devicons").get_icons()
+    local lualine = require("lualine")
+    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
-    local config = {
-      saved = { bg = colors.grey12, fg = colors.grey9 },
-      modified = { bg = colors.orange_washed, fg = colors.grey10 },
-      readonly = { bg = colors.red, fg = colors.fg },
+    local colors = {
+      blue = "#65D1FF",
+      green = "#3EFFDC",
+      violet = "#FF61EF",
+      yellow = "#FFDA7B",
+      red = "#FF4A4A",
+      fg = "#c3ccdc",
+      bg = "#112638",
+      inactive_bg = "#2c3043",
     }
 
-    local default_options = {
-      symbols = {
-        saved = " [✓]",
-        modified = " [~]",
-        readonly = " [✘]",
-        unnamed = "[No Name]",
-        newfile = "[New]",
+    local my_lualine_theme = {
+      normal = {
+        a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
+        b = { bg = colors.bg, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.fg },
+      },
+      insert = {
+        a = { bg = colors.green, fg = colors.bg, gui = "bold" },
+        b = { bg = colors.bg, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.fg },
+      },
+      visual = {
+        a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
+        b = { bg = colors.bg, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.fg },
+      },
+      command = {
+        a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
+        b = { bg = colors.bg, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.fg },
+      },
+      replace = {
+        a = { bg = colors.red, fg = colors.bg, gui = "bold" },
+        b = { bg = colors.bg, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.fg },
+      },
+      inactive = {
+        a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
+        b = { bg = colors.inactive_bg, fg = colors.semilightgray },
+        c = { bg = colors.inactive_bg, fg = colors.semilightgray },
       },
     }
 
-    function fname:init(options)
-      fname.super.init(self, options)
-
-      self.highlights = {
-        saved = highlight.create_component_highlight_group(config.saved, "filename_status_saved", self.options),
-        modified = highlight.create_component_highlight_group(
-          config.modified,
-          "filename_status_modified",
-          self.options
-        ),
-        readonly = highlight.create_component_highlight_group(
-          config.readonly,
-          "filename_status_readonly",
-          self.options
-        ),
-      }
-
-      self.options = vim.tbl_deep_extend("force", self.options or {}, default_options)
-
-      if self.options.color == nil then
-        self.options.color = ""
-      end
-    end
-
-    function fname:update_status()
-      local data
-      local symbols = {}
-
-      if _G.vim.tbl_contains(require("andre.core.excluded_filetypes")) then
-        return
-      end
-
-      -- Relative path
-      data = vim.fn.expand("%:~:.")
-      data = modules.utils.stl_escape(data)
-
-      if data == "" then
-        data = self.options.symbols.unnamed
-      end
-
-      local state
-      if vim.bo.modified then
-        state = self.options.symbols.modified
-      elseif vim.bo.modifiable == false or vim.bo.readonly == true then
-        state = self.options.symbols.readonly
-      else
-        state = self.options.symbols.saved
-      end
-      table.insert(symbols, state)
-
-      -- data = data .. "" .. (#symbols > 0 and "" .. table.concat(symbols, "") or "")
-      local icon = icons[vim.fn.expand("%:e")]
-      if icon then
-        icon = icon.icon
-      else
-        icon = ""
-      end
-
-      data = icon .. " " .. data .. "" .. (#symbols > 0 and "" .. table.concat(symbols, "") or "")
-
-      local hi_color
-      if vim.bo.modified then
-        hi_color = self.highlights.modified
-      elseif vim.bo.readonly then
-        hi_color = self.highlights.readonly
-      else
-        hi_color = self.highlights.saved
-      end
-
-      data = highlight.component_format_highlight(hi_color) .. data
-
-      return data
-    end
-
-    require("lualine").setup({
+    -- configure lualine with modified theme
+    lualine.setup({
       options = {
-        icons_enabled = true,
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-        -- disabled_filetypes = require("config.excluded_filetypes"),
-        disabled_filetypes = { "alpha", "Ranger" },
-        globalstatus = true,
-        always_divide_middle = true,
+        theme = my_lualine_theme,
       },
       sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch" },
-        lualine_c = { fname },
-        lualine_x = { "diff" },
-        lualine_y = {},
-        lualine_z = {},
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {},
-      },
-      winbar = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {},
+        lualine_x = {
+          {
+            lazy_status.updates,
+            cond = lazy_status.has_updates,
+            color = { fg = "#ff9e64" },
+          },
+          { "encoding" },
+          { "fileformat" },
+          { "filetype" },
+        },
       },
       extensions = {
         "nvim-tree",
-        "man",
         "lazy",
-        "trouble",
         "toggleterm",
       },
     })
