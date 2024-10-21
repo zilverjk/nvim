6075de948fb25d808af7950b5011cba6208a1d92
André Cruz <acruztalavera@gmail.com>
2024-10-20 19:31:42 -0500

    Corrección de italic en la syntaxis

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -2,6 +2,19 @@ local M = {}
 
 M.base46 = {
   theme = "tokyonight",
+  hl_override = {
+    Include = { italic = true },
+    ["@keyword"] = { italic = true },
+    ["@keyword.function"] = { italic = true },
+    ["@keyword.return"] = { italic = true },
+    ["@keyword.operator"] = { italic = true },
+    ["@keyword.import"] = { italic = true },
+    ["@keyword.exception"] = { italic = true },
+    ["@keyword.conditional"] = { italic = true },
+    ["@function"] = { italic = true },
+    ["@reference"] = { italic = true },
+    ["@comment"] = { italic = true },
+  },
 }
 
 M.ui = {
